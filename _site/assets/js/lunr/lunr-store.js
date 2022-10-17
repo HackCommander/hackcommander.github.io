33c3f4304da954309ce8b8c4dbe7c6778895926a
HackCommander <ivansantosmalpica.contact@gmail.com>
2022-10-17 05:30:08 +0200

    Post about time-based SQL injection in login form.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,8 +4,13 @@ var store = [{
         "tags": ["eLearnSecurity","eJPT","pentesting"],
         "url": "https://hackcommander.io/ejpt-certification-review/",
         "teaser":"https://hackcommander.io/assets/images/2022-09-01-ejpt-certification-review/ejpt-logo.png"},{
-        "title": "Reflected XSS bypassing an HTML tag removal sanitization",
-        "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 4. Report resolution :warning: This bug was found in a private bug bounty program in which it is not allowed to publish the vulnerabilities found. So this is a partial...","categories": ["Bug Bounty"],
+        "title": "Reflected XSS bypassing HTML tag removal sanitization",
+        "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 4. Report resolution :warning: This bug was reported in a private bug bounty program in which it is not allowed to publish the vulnerabilities found. So this is a partial...","categories": ["Bug Bounty"],
         "tags": ["web pentesting","OSINT","amass","httpx","gau","kxss","XSS","python","bypass","sanitization"],
         "url": "https://hackcommander.io/bug-bounty-1/",
+        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.png"},{
+        "title": "Time-based SQL injection in a login form",
+        "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 3.3. Is it possible to extract information from the database through this type of SQL injections? 4. Report resolution 5. Lessons learned :warning: This bug was reported in a private...","categories": ["Bug Bounty"],
+        "tags": ["web pentesting","OSINT","LeakIX","burpsuite","SQL injection","MySQL","information_schema","sqlmap","PHP info page"],
+        "url": "https://hackcommander.io/bug-bounty-2/",
         "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.png"}]
