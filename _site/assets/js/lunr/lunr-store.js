aaccf251579c398d83b04d29122be431ae5e01ba
HackCommander <ivansantosmalpica.contact@gmail.com>
2023-02-25 00:21:19 +0100

    Corrected the fields title and url of the file _config.yml.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,20 +2,20 @@ var store = [{
         "title": "eJPT certification review",
         "excerpt":"Summary 1. What is eJPT? 2. What will you learn and what previous knowledge do you need? 3. Is it useful for the curriculum? 4. Is it affordable? 5. How is the content? 5.1. Penetration Testing Prerequisites 5.2. Penetration Testing: Preliminary Skills &amp; Programming 5.3. Penetration Testing Basics 5.4. eJPT...","categories": ["Certification"],
         "tags": ["eLearnSecurity","eJPT","pentesting"],
-        "url": "https://hackcommander.io/ejpt-certification-review/",
-        "teaser":"https://hackcommander.io/assets/images/2022-09-01-ejpt-certification-review/ejpt-logo.png"},{
+        "url": "https://hackcommander.github.io/ejpt-certification-review/",
+        "teaser":"https://hackcommander.github.io/assets/images/2022-09-01-ejpt-certification-review/ejpt-logo.png"},{
         "title": "Reflected XSS bypassing HTML tag removal sanitization",
         "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 3.2.1. Sanitization algorithm based on a boolean variable 3.2.2. Sanitization algorithm based on a data structure 3.2.3. Code of the algorithms 4. Report resolution 5. Lessons learned :warning: This bug...","categories": ["Bug Bounty"],
         "tags": ["web pentesting","OSINT","amass","httpx","gau","kxss","XSS","python","bypass","sanitization"],
-        "url": "https://hackcommander.io/bug-bounty-1/",
-        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.jpg"},{
+        "url": "https://hackcommander.github.io/bug-bounty-1/",
+        "teaser":"https://hackcommander.github.io/assets/images/general/bug-bounty.jpg"},{
         "title": "Time-based SQL injection in a login form",
         "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 3.3. Is it possible to extract information from the database through this type of SQL injections? 4. Report resolution 5. Lessons learned :warning: This bug was reported in a private...","categories": ["Bug Bounty"],
         "tags": ["web pentesting","OSINT","LeakIX","burpsuite","SQL injection","MySQL","information_schema","sqlmap","PHP info page"],
-        "url": "https://hackcommander.io/bug-bounty-2/",
-        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.jpg"},{
+        "url": "https://hackcommander.github.io/bug-bounty-2/",
+        "teaser":"https://hackcommander.github.io/assets/images/general/bug-bounty.jpg"},{
         "title": "How to bypass the HttpOnly flag via the PHP info page to exfiltrate the user cookies during an XSS exploitation",
         "excerpt":"Summary 1. Setting up the environment 2. Hands-on! 2.1. Trying to exfiltrate the cookies through the usual method 2.2. Exfiltrating the cookies bypassing the HttpOnly flag through the PHP info page 2.3. Using my Github tool to generate an improved payload 2.4. Important notes 3. Conclusions PHP info page disclosure...","categories": ["Pentesting article"],
         "tags": ["web pentesting","burpsuite","base64","metasploitable 2","XSS","exfiltrate cookies","session hijacking","PHP info page","bypass"],
-        "url": "https://hackcommander.io/pentesting-article-1/",
-        "teaser":"https://hackcommander.io/assets/images/2022-11-12-pentesting-article-1/phpinfo-dvwa-1.png"}]
+        "url": "https://hackcommander.github.io/pentesting-article-1/",
+        "teaser":"https://hackcommander.github.io/assets/images/2022-11-12-pentesting-article-1/phpinfo-dvwa-1.png"}]
