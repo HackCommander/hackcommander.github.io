6bfb6d180c7adfe14a7331a8c3976665efa58480
HackCommander <ivansantosmalpica.contact@gmail.com>
2023-02-24 19:55:34 +0100

    New illustration added to my blog for bug bounty posts.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,12 +8,12 @@ var store = [{
         "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 3.2.1. Sanitization algorithm based on a boolean variable 3.2.2. Sanitization algorithm based on a data structure 3.2.3. Code of the algorithms 4. Report resolution 5. Lessons learned :warning: This bug...","categories": ["Bug Bounty"],
         "tags": ["web pentesting","OSINT","amass","httpx","gau","kxss","XSS","python","bypass","sanitization"],
         "url": "https://hackcommander.io/bug-bounty-1/",
-        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.png"},{
+        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.jpg"},{
         "title": "Time-based SQL injection in a login form",
         "excerpt":"Summary 1. Asset discovery 2. Vulnerability discovery 3. Vulnerability exploitation 3.1. Steps of exploitation 3.2. Why does the payload work? 3.3. Is it possible to extract information from the database through this type of SQL injections? 4. Report resolution 5. Lessons learned :warning: This bug was reported in a private...","categories": ["Bug Bounty"],
         "tags": ["web pentesting","OSINT","LeakIX","burpsuite","SQL injection","MySQL","information_schema","sqlmap","PHP info page"],
         "url": "https://hackcommander.io/bug-bounty-2/",
-        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.png"},{
+        "teaser":"https://hackcommander.io/assets/images/general/bug-bounty.jpg"},{
         "title": "How to bypass the HttpOnly flag via the PHP info page to exfiltrate the user cookies during an XSS exploitation",
         "excerpt":"Summary 1. Setting up the environment 2. Hands-on! 2.1. Trying to exfiltrate the cookies through the usual method 2.2. Exfiltrating the cookies bypassing the HttpOnly flag through the PHP info page 2.3. Using my Github tool to generate an improved payload 2.4. Important notes 3. Conclusions PHP info page disclosure...","categories": ["Pentesting article"],
         "tags": ["web pentesting","burpsuite","base64","metasploitable 2","XSS","exfiltrate cookies","session hijacking","PHP info page","bypass"],
